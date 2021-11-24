5b2fbef748c6524d522ad7ff2b6d1589d15d0582
soo9028 <sj.kim28@daum.net>
2021-11-24 17:21:18 +0900

    Last commit

--- navigators/MainDrawerNav.js
+++ navigators/MainDrawerNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer'
 import MovieStackNav from './MovieStackNav';
+import CommunityTabNav from './CommunityTabNav';
 
 
 const Drawer= createDrawerNavigator()
@@ -9,6 +10,7 @@ const MainDrawerNav= ()=>{
     return (
         <Drawer.Navigator screenOptions={{headerShown:false}}>
             <Drawer.Screen name="MovieStackNav" component={MovieStackNav}></Drawer.Screen>
+            <Drawer.Screen name="CommunityTabNav" component={CommunityTabNav}></Drawer.Screen>
         </Drawer.Navigator>
     )
 }
